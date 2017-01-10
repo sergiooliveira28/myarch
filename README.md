667a6322727ca55b6b1f14ac0f9bf13fb4b50fb6
Sérgio Oliveira <sergio@asusk56cb.lan>
2017-01-10 20:10:46 +0000

    1st commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# myarch
+Some Arch Linux backup files
---
+++ mouse.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Simple sh for natural scrolling
+echo `xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Scrolling Distance" -78 -78`
